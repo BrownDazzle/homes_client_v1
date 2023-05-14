abcf7d38190b513cd3c5e34da4c11301d2cdb38b
BrownDazzle <actscloud28@gmail.com>
2023-05-15 00:03:50 +0200

    first commit

---
+++ src/data.js
@@ -0,0 +1,109 @@
+import place1 from "./assets/unsplash1.jpg"
+import place2 from "./assets/unsplash2.jpg"
+import place3 from "./assets/unsplash3.jpg"
+import place4 from "./assets/unsplash4.jpg"
+import place5 from "./assets/unsplash5.jpg"
+import place6 from "./assets/unsplash6.jpg"
+import place7 from "./assets/unsplash7.jpg"
+import place8 from "./assets/unsplash8.jpg"
+import place9 from "./assets/unsplash9.jpg"
+import place10 from "./assets/unsplash10.jpg"
+import place11 from "./assets/unsplash11.jpg"
+import place12 from "./assets/unsplash12.jpg"
+import place13 from "./assets/unsplash13.jpg"
+import place14 from "./assets/unsplash14.jpg"
+import place15 from "./assets/unsplash15.jpg"
+import place16 from "./assets/unsplash16.jpg"
+import place17 from "./assets/unsplash17.jpg"
+import place18 from "./assets/unsplash18.jpg"
+import place19 from "./assets/unsplash19.jpg"
+
+const places = [
+    {
+        _id: 1,
+        photos: [place1, place2, place3, place4],
+        address: "18/8 Northgate City",
+        title: "A luxury home",
+        price: 25000
+    },
+    {
+        _id: 2,
+        photos: [place3, place4, place5, place6],
+        address: "18/8 Northgate City",
+        title: "A luxury home",
+        price: 25000
+    },
+    {
+        _id: 3,
+        photos: [place6, place7, place8, place9],
+        address: "18/8 Northgate City",
+        title: "A luxury home",
+        price: 25000
+    },
+    {
+        _id: 4,
+        photos: [place9, place10, place11, place12],
+        address: "18/8 Northgate City",
+        title: "A luxury home",
+        price: 25000
+    },
+    {
+        _id: 5,
+        photos: [place13, place14, place15, place16],
+        address: "18/8 Northgate City",
+        title: "A luxury home",
+        price: 25000
+    },
+    {
+        _id: 6,
+        photos: [place16, place17, place18, place19],
+        address: "18/8 Northgate City",
+        title: "A luxury home",
+        price: 25000
+    },
+    {
+        _id: 7,
+        photos: [place13, place2, place3, place4],
+        address: "18/8 Northgate City",
+        title: "A luxury home",
+        price: 25000
+    },
+    {
+        _id: 8,
+        photos: [place14, place4, place5, place6],
+        address: "18/8 Northgate City",
+        title: "A luxury home",
+        price: 25000
+    },
+    {
+        _id: 9,
+        photos: [place16, place7, place8, place9],
+        address: "18/8 Northgate City",
+        title: "A luxury home",
+        price: 25000
+    },
+    {
+        _id: 10,
+        photos: [place17, place10, place11, place12],
+        address: "18/8 Northgate City",
+        title: "A luxury home",
+        price: 25000
+    },
+    {
+        _id: 11,
+        photos: [place18, place14, place15, place16],
+        address: "18/8 Northgate City",
+        title: "A luxury home",
+        price: 25000
+    },
+    {
+        _id: 12,
+        photos: [place19, place17, place18, place19],
+        address: "18/8 Northgate City",
+        title: "A luxury home",
+        price: 25000
+    },
+]
+
+
+export { places }
